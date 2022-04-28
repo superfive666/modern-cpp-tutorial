6e448e9cebb5b2b0d44185ebb325f45dc02875de
Superfive <chao.wu@aquariux.com>
2022-04-28 17:08:49 +0800

    Adding clangd configuration to use c++20 and exercise chapter 7.1

--- exercises/7/7.1/main.cpp
+++ exercises/7/7.1/main.cpp
@@ -10,41 +10,41 @@
 
 #include <iostream> // std::cout, std::endl
 
-#include <vector>   // std::vector
-#include <string>   // std::string
-#include <future>   // std::future
-#include <thread>   // std::this_thread::sleep_for
-#include <chrono>   // std::chrono::seconds
+#include <chrono> // std::chrono::seconds
+#include <future> // std::future
+#include <string> // std::string
+#include <thread> // std::this_thread::sleep_for
+#include <vector> // std::vector
 
 #include "thread_pool.hpp"
 
-int main()
-{
-    // create a thread pool with max. 4 concurrency threads
-    ThreadPool pool(4);
-    // create execution results list
-    std::vector< std::future<std::string> > results;
+int main() {
+  // create a thread pool with max. 4 concurrency threads
+  ThreadPool pool(4);
+  // create execution results list
+  std::vector<std::future<std::string>> results;
 
-    // start eight thread task
-    for(int i = 0; i < 8; ++i) {
-        // add all task to result list
-        results.emplace_back(
-            // ass print task to thread pool
-            pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
-                // wait a sec when the previous line is out
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                // keep output and return the status of execution
-                std::cout << "world " << i << std::endl;
-                return std::string("---thread ") + std::to_string(i) + std::string(" finished.---");
-            })
-        );
-    }
+  // start eight thread task
+  for (int i = 0; i < 8; ++i) {
+    // add all task to result list
+    results.emplace_back(
+        // ass print task to thread pool
+        pool.enqueue([i] {
+          std::cout << "hello " << i << std::endl;
+          // wait a sec when the previous line is out
+          std::this_thread::sleep_for(std::chrono::seconds(1));
+          // keep output and return the status of execution
+          std::cout << "world " << i << std::endl;
 
-    // outputs
-    for(auto && result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
+          return std::string("---thread ") + std::to_string(i) +
+                 std::string(" finished.---");
+        }));
+  }
 
-    return 0;
+  // outputs
+  for (auto &&result : results)
+    std::cout << result.get() << std::endl;
+  std::cout << std::endl;
+
+  return 0;
 }
